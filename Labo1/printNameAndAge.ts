afa4d6254624475e53bbaffb13b5d86fe60662cf
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-02-10 13:49:10 +0100

    Labo1 gedaan

---
+++ Labo1/printNameAndAge.ts
@@ -0,0 +1,5 @@
+let voornaam :string=  "Hugues";
+let achternaam :string= "Kiendrebeogo";
+let leeftijd :number= 25;
+
+console.log(`Ik ben ${voornaam} ${achternaam} en ik ben  ${leeftijd} jaar oud`);
