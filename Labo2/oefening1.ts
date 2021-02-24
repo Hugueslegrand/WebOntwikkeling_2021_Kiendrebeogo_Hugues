08b385c20c2b514e5d518e6ade6ad6c7eced55b3
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-02-24 14:57:34 +0100

    oefening 1 2x aangepast

--- Labo2/oefening1.ts
+++ Labo2/oefening1.ts
@@ -1,7 +1,7 @@
 let getallen: Array<number> = [];
 
 for (let i = 0; i<=100;){
-    getallen[i] = i+100;
+   getallen[i]=i+100;
    while(getallen[i]<=200){
     console.log(getallen[i]);
     i++;
@@ -9,4 +9,9 @@ for (let i = 0; i<=100;){
 
 }
 
- 
+ for (let index = 0; index <=100; index++) {
+    getallen[index]=index+100;
+    if (getallen[index]%3==0) {
+         console.log(getallen[index]);
+    }  
+ }
