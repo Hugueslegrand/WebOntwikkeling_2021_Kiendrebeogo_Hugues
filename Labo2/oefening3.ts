af805fc1ed370f83305366e551f66b5c597a5452
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-02-24 15:10:50 +0100

    oefening 3 laatste stuk met ? toegevoegd

--- Labo2/oefening3.ts
+++ Labo2/oefening3.ts
@@ -9,3 +9,11 @@ for (let i = 0; i <= 10; i++) {
     }
     
 }
+
+for (let i = 0; i <= 10; i++) {
+    veelvouden[i]= i*7;
+
+    console.log((veelvouden[i] %5 == 0 || veelvouden[i] %3 == 0) 
+    &&
+    !(veelvouden[i] %5 == 0 && veelvouden[i] %3 == 0)?veelvouden[i]:"")
+}
