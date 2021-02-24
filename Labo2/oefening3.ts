f5f238da109475ebc178d34bea690cc735ed6d64
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-02-24 13:54:43 +0100

    oefening 3 toegevoegd

---
+++ Labo2/oefening3.ts
@@ -0,0 +1,11 @@
+let veelvouden: Array<number> = [];
+
+for (let i = 0; i <= 10; i++) {
+   veelvouden[i]= i*7;
+    if ((veelvouden[i] %5 == 0 || veelvouden[i] %3 == 0) 
+    &&
+    !(veelvouden[i] %5 == 0 && veelvouden[i] %3 == 0) ) {
+        console.log(veelvouden[i]);
+    }
+    
+}
