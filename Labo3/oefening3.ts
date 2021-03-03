23dfa69b13b0f029c48abae4c7b79ab3baad6064
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-03-03 13:50:17 +0100

    Labo 3 oefening 3 was nog niet af het is nu bijgewerkt

--- Labo3/oefening3.ts
+++ Labo3/oefening3.ts
@@ -1,5 +1,14 @@
 const multiplyText = (amount: number, text: string)=>{
 let tijdelijkeString: string = " ";
+if (text=="") { 
+    
+    text= "default ";
+    
+    for (let index: number = 0; index < amount; index++) {
+        tijdelijkeString += text;
+        
+    }
+}else
     for (let index: number = 0; index < amount; index++) {
         tijdelijkeString += text;
         
@@ -7,5 +16,5 @@ let tijdelijkeString: string = " ";
      return tijdelijkeString;
 }
 
-let result: string = multiplyText(3,"flower ");
+let result: string = multiplyText(3,"");
     console.log("result is"+result)
