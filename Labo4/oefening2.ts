1a7eeb04b58aabfef6a35fed7f48fc536a755b29
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-03-10 18:10:18 +0100

    oefening 2 b functions af

--- Labo4/oefening2.ts
+++ Labo4/oefening2.ts
@@ -6,7 +6,7 @@ interface Movie {
     actors: string[],
     metascore: number,
     seen: boolean
-}
+};
 
 const thematrix :Movie= {
         title: "The Matrix",
@@ -14,7 +14,7 @@ const thematrix :Movie= {
         actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
         metascore: 73,
         seen: true
-}
+};
 
 const myfavoritemovie :Movie = {
         title: "Requiem for a Dream",
@@ -22,7 +22,7 @@ const myfavoritemovie :Movie = {
         actors: ["Ellen Butstyn", "Jared Leto", "Jennifer Connelly", "Marlon Wayans"],
         metascore: 68,
         seen: true
-}
+};
 
 const myworstmovie :Movie = {
     title: "The Last Airbender",
@@ -30,6 +30,30 @@ const myworstmovie :Movie = {
     actors: ["Noah Ringer", "Nicola Peltz", "Dev Patel", "Jackson Rathbone", "Shaun Toub"],
     metascore: 20,
     seen: true
-}
+};
 
 console.log(thematrix.actors);
+
+/*Oefening 2b: Functions*/
+
+const wasMovieMadeInThe90s = (movie:Movie):boolean=>{
+    if (movie.year <2000) {
+        return true;
+    }else {
+        return false;
+    }
+};
+
+console.log(wasMovieMadeInThe90s(thematrix));
+console.log(wasMovieMadeInThe90s(myfavoritemovie));
+
+const averageMetaScore =(movies:Movie[]):number =>{
+    let average: number = ((thematrix.metascore + myfavoritemovie.metascore + myworstmovie.metascore)/3);
+    return average;
+}
+console.log(averageMetaScore([myworstmovie,thematrix,myfavoritemovie]));
+
+const fakeMetaScore =(movies:Movie):number =>{
+    return movies.metascore = 34;
+}
+console.log("New metascore = "+fakeMetaScore(myfavoritemovie));
