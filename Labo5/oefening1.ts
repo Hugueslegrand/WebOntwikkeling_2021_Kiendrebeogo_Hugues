5c100af7059cd53ad7764b4c4cafc0eb907e43f2
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-03-17 13:11:43 +0100

    oefening1 af

--- Labo5/oefening1.ts
+++ Labo5/oefening1.ts
@@ -1,14 +1,5 @@
-interface Print{
-    (amount:number, text:string):void
-}
-const printStuff : Print = (amount, text) => console.log(`Hello ${text}, you are number ${amount}`);
+const printStuff = (amount:number, text:string):void => console.log(`Hello ${text}, you are number ${amount}`);
 
-interface TwoArray{
-    (element1:string, element2: string): string[]
-}
-const twoDArray:TwoArray = (element1, element2) => [element1, element2];
+const twoDArray = (element1:string, element2:string):string[] => [element1, element2];
 
-interface NumberToStrng{
-    (number: number): string
-}
-const numberToString: NumberToStrng= (number) => `${number}`;
+const numberToString = (number:number):string => `${number}`;
