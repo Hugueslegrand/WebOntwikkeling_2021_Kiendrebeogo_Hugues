6d0eacdd30b18e43b932533db70917f8ebcbe7d0
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-03-24 14:05:25 +0100

    oefening3 aangepast voor evaluatie

--- Labo5/oefening3.ts
+++ Labo5/oefening3.ts
@@ -3,13 +3,22 @@ interface Calculation {(a:number, b:number):number}
 const printCalculationResult = (a:number,b:number,func:Calculation)=>{
     if (func==add) {
         console.log(add(a,b));
-    }else
+    }else 
     console.log(mult(a,b));
    
 }
 
 const mult = (a:number,b:number)=> a*b;
 const add = (a:number,b:number)=> a+b;
+const devide= (a:number,b:number)=> {
+    if (b===0) {
+    return  -1 ;
+    }
+    else
+    {
+    return a/b;}
+
+};
 
 printCalculationResult(2,4,add);
 printCalculationResult(2,4,mult);
