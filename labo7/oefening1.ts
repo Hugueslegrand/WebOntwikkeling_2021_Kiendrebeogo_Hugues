fa94382fb45b51f559e1c525e1136f905a5457a3
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-04-21 13:21:01 +0200

    slowSum functie aanroeping

--- labo7/oefening1.ts
+++ labo7/oefening1.ts
@@ -13,3 +13,14 @@ const slowMult = (a: number, b: number) => {
         },1500)
     });
 }
+const slowDiv = (a: number, b: number) => {
+    return new Promise<number>((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a/b);
+        },2000)
+    });
+}
+
+slowSum(1,5).then((result) => { console.log("1 + 5 = "+result)}
+);
+
