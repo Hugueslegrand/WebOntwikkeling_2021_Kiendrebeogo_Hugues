58ce09c7f7d98d19fbfa8dbfbcc48115597d7e55
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-04-21 13:28:25 +0200

    you cannot divide by zero

--- labo7/oefening1.ts
+++ labo7/oefening1.ts
@@ -15,16 +15,22 @@ const slowMult = (a: number, b: number) => {
 }
 const slowDiv = (a: number, b: number) => {
     return new Promise<number>((resolve, reject) => {
-        setTimeout(() => {
+        if (a !=0 && b != 0) {
+            setTimeout(() => {
             resolve(a/b);
         },2000)
+        }else{
+            console.log("You cannot divide by zero")
+        }  
     });
 }
 
-slowSum(1,5).then((result) => { console.log("1 + 5 = "+result)}
+slowDiv(0,4).then((result) => { console.log(result); });
+
+slowSum(1,5).then((result) => { console.log("1 + 5 = "+result);}
 );
 
 slowSum(1,5).then((result) => { 
-    slowMult(result,2).then((result) => { console.log("(1 + 5) * 2 = "+result)});
+    slowMult(result,2).then((result) => { console.log("(1 + 5) * 2 = "+result);});
     }
 );
