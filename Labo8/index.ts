6020b35fde14fc715b8518e7f3c4338569f3d880
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-04-28 14:18:39 +0200

    express app

--- Labo8/index.ts
+++ Labo8/index.ts
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const ejs= require('ejs');
+app.set('view engine', 'ejs');
 app.set('port', 3000);
 
+
+const thisisme =
+    {   
+        name: 'Hugues',
+        age: 25,
+        profilepic: `https://scontent-bru2-1.xx.fbcdn.net/v/t1.6435-9/67539739_2412892925399406_6333425797646254080_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=4cTruBf6Z7YAX9bYEHp&_nc_ht=scontent-bru2-1.xx&oh=facc6a3d1e0a4a708584e6dfa82c0a50&oe=60AD673A`
+    };
+
 app.get('/',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('HellobWorld')
+    
+    res.render('index');
 });
 app.get('/whoami',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    
+    res.render('whoami');
+
 });
 app.get('/whoamijson',(req:any,res:any)=>{
     res.type('text/html');
