86c98963bfbdcd0604173a2db6a5f3a26c87e0ca
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-05-19 13:17:15 +0200

    labo 10 geintegreerd

--- labo11/index.ts
+++ labo11/index.ts
@@ -8,16 +8,47 @@ app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 app.set('port', 3000);
 
-const movies = [
-        {name: "The Matrix", myScore: 90},
-        {name: "Pulp Fuction", myScore: 100},
-        {name: "Monster Hunter", myScore: 5},
-        {name: "Blade Runner", myScore: 100}
-];
+const { MongoClient } = require('mongodb');
+
+const uri =
+    "mongodb+srv://Hugues:APHogeschool@cluster0.c6h6v.mongodb.net/WebOntwikkeling?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+
 /* app.get('/',(req:any,res:any)=>{
     res.type('text/html');
     res.send('Hello World');
 }); */
+interface MOVIES {
+    name: string,
+    myScore: number,
+    timesViewed: number
+}
+const movies = [
+   {
+       
+   }
+];
+
+const callDB = async () => {
+    try {
+        await client.connect();
+        //Data uit de database in de array movies steken
+        let cursor = await client.db('WebOntwikkeling').collection('Movies').find({});
+        let AllMovies: MOVIES[] = await cursor.toArray();
+        for (let index = 0; index < AllMovies.length; index++) {
+            movies[index] = AllMovies[index];
+        }
+
+    }
+    catch (exc) {
+        console.log(exc);
+    }
+    finally {
+        await client.close();
+    }
+}
+callDB();
 app.get('/', (req: any, res: any) => {
 
     res.render('landingPage');
