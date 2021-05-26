2d23cbd5d69a736332f1ba64781bc03e71e997dc
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-05-26 12:46:19 +0200

    add movie

--- labo11/index.ts
+++ labo11/index.ts
@@ -8,7 +8,7 @@ app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 app.set('port', 3000);
 
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const uri =
     "mongodb+srv://Hugues:APHogeschool@cluster0.c6h6v.mongodb.net/WebOntwikkeling?retryWrites=true&w=majority";
@@ -20,14 +20,13 @@ const client = new MongoClient(uri, { useUnifiedTopology: true });
     res.send('Hello World');
 }); */
 interface MOVIES {
+    _id?: number,
     name: string,
     myScore: number,
     timesViewed: number
 }
-const movies = [
-   {
-       
-   }
+const movies: MOVIES[] = [
+
 ];
 
 const callDB = async () => {
@@ -36,10 +35,24 @@ const callDB = async () => {
         //Data uit de database in de array movies steken
         let cursor = await client.db('WebOntwikkeling').collection('Movies').find({});
         let AllMovies: MOVIES[] = await cursor.toArray();
+
+        for (let index = 0; index < movies.length; index++) {
+            await client.db('WebOntwikkeling').collection('Movies').updateOne({ name: movies[index].name }
+                ,
+                {
+                    $set: {
+                        name: movies[index].name,
+                        myScore: movies[index].myScore,
+                        timesViewed: movies[index].timesViewed
+                    }
+                },
+                { upsert: true });
+        }
         for (let index = 0; index < AllMovies.length; index++) {
             movies[index] = AllMovies[index];
+           
         }
-
+        
     }
     catch (exc) {
         console.log(exc);
@@ -50,26 +63,42 @@ const callDB = async () => {
 }
 
 app.get('/', (req: any, res: any) => {
-    callDB();
+   
     res.render('landingPage');
 });
+
 app.get('/movies', (req: any, res: any) => {
     
-    res.render('movies',{movies});
+    res.render('movies', { movies });
 });
-app.get('/movies/:index',(req:any,res:any)=>{
-    let index = req.params.index;
-   
-    res.render('movie',movies[index]);
-})
-app.get('/addmovie',(req:any,res:any)=>{
 
+app.get('/movies/:index', (req: any, res: any) => {
+    let index = req.params.index;
+    res.render('movie', movies[index]);
+})
+
+app.get('/removemovie/:index', (req: any, res: any) => {
+    let index = req.params.index;
+    async () => {
+            let cursor = await client.db('WebOntwikkeling').collection('Movies').find({});
+            let AllMovies: MOVIES[] = await cursor.toArray();
+            await client.db('WebOntwikkeling').collection('Movies').deleteOne(
+                {_id: AllMovies[index]._id})
+    }
+    res.render('movies', { movies });
+})
+
+app.get('/addmovie', (req: any, res: any) => {
+    
     res.render('addMovie');
 })
-app.post('/addmovie',(req:any,res:any)=>{
-    movies.push({name: req.body.name, myScore: req.body.myScore});
-    res.render('addMovie');
-   
+
+app.post('/addmovie', (req: any, res: any) => {
+    callDB();
+    movies.push({ name: req.body.name, myScore: req.body.myScore, timesViewed: 0 });
+    
+    res.render('movies',{movies});
 })
+
 app.listen(app.get('port'),
     () => console.log('[server] http://localhost:' + app.get('port')));
